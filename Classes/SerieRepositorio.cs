6eacecd8bb7648aac7b997901978f46e6dfe831c
MicaellNascimento <Micaell.araujo.ti@gmail.com>
2021-07-03 00:08:20 -0300

    Create Interfaces

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using DIO.Series.Interfaces;
+namespace DIO.Series
+{
+    public class SeriesRepositorio : iRepositorio<Serie>
+    {
+        public void atualiza(int id, Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exclui(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
